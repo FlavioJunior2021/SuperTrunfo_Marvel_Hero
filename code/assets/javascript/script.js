5968f039397a1fa75d8a8b913f38d76e5ce6b27c
Flávio Jr <89926456+FlavioJunior2021@users.noreply.github.com>
2021-09-21 20:39:13 -0300

    Update script.js
    Adicionando personagens

--- code/assets/javascript/script.js
+++ code/assets/javascript/script.js
@@ -1,3 +1,76 @@
 //Verificar o que acontece caso você não selecione nenhum dos atributos e como solucionar
 //Utilizar personagens que você gosta para criar as cartas e compartilhe com o pessoal lá na comunidade do Discord!
 //Adicionar a imagem do personagem assim que você selecionar a carta dele
+
+var carta1 = {
+    imagem: "https://i.pinimg.com/474x/80/03/cd/8003cddfc723914e236f047793f25d7d.jpg",
+    nome: "Homem Aranha",
+    atributos:{
+        ataque:9,
+        defesa:6,
+        acrobacias:10,
+        magia:0,
+        forca: 9,
+        inteligencia:9
+    }
+};
+var carta2 = {
+    imagem: "https://i.pinimg.com/474x/6a/48/24/6a48243db07f036cf67372bcb40c3268.jpg",
+    nome: "Homem de Ferro",
+    atributos:{
+        ataque:7,
+        defesa:9,
+        acrobacias:8,
+        magia:0,
+        forca: 8,
+        inteligencia:10
+    }
+};
+var carta3 = {
+    imagem: "https://www.hqzona.com.br/wp-content/uploads/2018/03/fmt_94_24_kehp1.jpg",
+    nome: "Capitão America",
+    atributos:{
+        ataque:8,
+        defesa:10,
+        acrobacias:7,
+        magia:0,
+        forca: 9,
+        inteligencia:6
+    }
+};
+var carta4 = {
+    imagem: "https://i.pinimg.com/originals/86/ef/54/86ef544581734054623ef7cf15fe0abd.jpg",
+    nome: "Hulk!",
+    atributos:{
+        ataque:10,
+        defesa:10,
+        acrobacias:5,
+        magia:0,
+        forca: 10,
+        inteligencia:3
+    }
+};
+var carta5 = {
+    imagem: "https://static.wikia.nocookie.net/marvel/images/7/79/Black_Widow_Vol_6_12_Textless.jpg/revision/latest/top-crop/width/360/height/450?cb=20181108015928&path-prefix=pt-br",
+    nome: "Viuva Negra",
+    atributos:{
+        ataque:7,
+        defesa:7,
+        acrobacias:9,
+        magia:0,
+        forca: 5,
+        inteligencia:8
+    }
+};
+var carta6 = {
+    imagem: "https://i1.wp.com/chefaodefase.com.br/Pt-br/wp-content/uploads/2018/02/Dr-Estranho5.jpg",
+    nome: "Doutor Estranho",
+    atributos:{
+        ataque:8,
+        defesa:9,
+        acrobacias:5,
+        magia:10,
+        forca: 5,
+        inteligencia:9
+    }
+};
